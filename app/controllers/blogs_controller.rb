619bb8cd6ccdb75f8c06df6f56329d0a8a11b82c
seira_tanaka <seira_tanaka@diveintocode.jp>
2017-06-27 08:57:36 +0000

    first commit

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,64 @@
+class BlogsController < ApplicationController
+ before_action :set_blog,only:[:edit,:update,:destroy]
+
+  def index
+    # 処理が実行される
+    # @がついてるとビューに送れる
+    # =は代入
+    # Blogはモデルのクラス名を指している
+    # 実行するとSELECT * FROM blogs;
+    @blogs = Blog.all
+    # render :indexが実は書いてある
+  end
+  
+  def new
+   if params[:back]
+    @blog = Blog.new(blogs_params)
+   else
+    @blog = Blog.new
+   end
+  end
+  
+  def create
+    @blog=Blog.new(blogs_params)
+   if @blog.save
+    redirect_to blogs_path,notice:"ブログを作成しました！"
+   else
+    render'new'
+   end
+  end
+  
+  def edit
+    @blog = Blog.find(params[:id])
+  end
+  
+  def update
+    @blog = Blog.find(params[:id])
+   if @blog.update(blogs_params)
+    redirect_to blogs_path,notice:"ブログを更新しました！"
+   else
+    render'edit'
+   end
+  end  
+  
+  def destroy
+    @blog = Blog.find(params[:id])
+    @blog.destroy
+    redirect_to blogs_path,notice:"ブログを削除しました！"
+  end
+  
+  def confirm
+    @blog = Blog.new(blogs_params)
+    render :new if @blog.invalid?
+  end
+  
+  private
+  
+  def blogs_params
+    params.require(:blog).permit(:title,:content)
+  end
+  
+  def set_blog
+    @blog=Blog.find(params[:id])
+  end
+end
